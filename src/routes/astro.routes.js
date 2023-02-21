0505acc8c3a2293c1e902a392abc80972e6dc77c
Sebas <sebastiansilvan97@gmail.com>
2023-02-20 19:51:09 -0500

    endpoint to  getAllAstros created

--- src/routes/astro.routes.js
+++ src/routes/astro.routes.js
@@ -6,4 +6,5 @@ const router = Router();
 router.get("/type/:id", astroController.getAstrosByType);
 router.get("/types", astroController.getTypeAstros);
 router.get("/one/:id", astroController.getAstroById);
+router.get("/all", astroController.getAllAstros);
 export default router;
